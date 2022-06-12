f6eb6c8bc2522bf8e4b498c54aa4b39a0739e8c6
zamia1 <zamia.chowdhury@gmail.com>
2022-06-12 10:17:41 -0700

    fixed bugs

--- app.py
+++ app.py
@@ -1,14 +1,8 @@
 from flask import Flask, render_template, request
 import os
-import stat
-#import pdb
 import webbrowser
 app = Flask(__name__)
 import os,subprocess
-
-
-# Make the WSGI interface available at the top level so wfastcgi can get it.
-#wsgi_app = app.wsgi_app
 
 FILE_SYSTEM_ROOT =os.getcwd()
 
@@ -17,49 +11,31 @@ FILE_SYSTEM_ROOT =os.getcwd()
 def browser(): 
     itemList = os.listdir(FILE_SYSTEM_ROOT)
     data_lines=[]
-    #pdb.set_trace()
     if request.method == 'POST': 
-        # pdb.set_trace()
         result = request.form['name']
         data_lines = subprocess.check_output(result, shell=True).decode('utf-8').splitlines()
         return render_template('final.html',data=data_lines, itemList=itemList)
     else:
         return render_template('browse.html', itemList=itemList)
 
-
-@app.route('/browser/',methods=["POST","GET"])   
+  
 @app.route('/browser/<path:urlFilePath>',methods=["POST","GET"]) 
-def browse(urlFilePath=None):
-    #if request.method == 'POST':
+def browse(urlFilePath):
     data_lines=[]
-    # pdb.set_trace()
-    itemList = os.listdir(FILE_SYSTEM_ROOT)
-    if urlFilePath is not None:
-        nestedFilePath = os.path.join(FILE_SYSTEM_ROOT, urlFilePath)
-        if os.path.isdir(nestedFilePath):
-            # pdb.set_trace()
-            itemList = os.listdir(nestedFilePath)
-            fileProperties = {"filepath": nestedFilePath}
-            if not urlFilePath.startswith("/"):
-                urlFilePath = "/" + urlFilePath
-            itemList = os.listdir(nestedFilePath) 
-            return render_template('final.html', urlFilePath=urlFilePath,data=data_lines, itemList=itemList)
-        if os.path.isfile(nestedFilePath):
-            # pdb.set_trace()
-            itemList = os.listdir(FILE_SYSTEM_ROOT)
-            if not urlFilePath.startswith("/"):
-                urlFilePath = "/" + urlFilePath
-            with open(nestedFilePath, 'r') as f: 
-                return render_template('final.html', urlFilePath=urlFilePath,data=data_lines,itemList=itemList, text=f.read())
-    else:
-        if request.method == 'POST': 
-            # pdb.set_trace()
-            result = request.form['name']
-            data_lines = subprocess.check_output(result, shell=True).decode('utf-8').splitlines()
-            return render_template('final.html',data=data_lines, itemList=itemList)
-        else:
-            return render_template('browse.html', itemList=itemList)
-
+    os.path.join(FILE_SYSTEM_ROOT, urlFilePath)   
+    nestedFilePath = os.path.join(FILE_SYSTEM_ROOT, urlFilePath)
+    if os.path.isdir(nestedFilePath):
+        fileProperties = {"filepath": nestedFilePath}
+        if not urlFilePath.startswith("/"):
+            urlFilePath = "/" + urlFilePath
+        itemList = os.listdir(nestedFilePath) 
+        return render_template('final.html', urlFilePath=urlFilePath,data=data_lines, itemList=itemList)
+    elif os.path.isfile(nestedFilePath):
+        itemList = os.listdir(os.path.dirname(nestedFilePath))
+        if not urlFilePath.startswith("/"):
+            urlFilePath = "/" + urlFilePath
+        with open(nestedFilePath, 'r') as f: 
+            return render_template('final.html', urlFilePath=urlFilePath,data=data_lines,itemList=itemList, text=f.read())
 
 if __name__ == '__main__':
     app.run()
